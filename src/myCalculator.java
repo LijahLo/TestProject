7e2f0888c7004fedcb2aecaf22894e4f845f8c1b
Lijahlo <londone3@newpaltz.edu>
2024-04-15 10:33:43 -0400

    Initial Commit for this project

--- src/myCalculator.java
+++ src/myCalculator.java
@@ -2,6 +2,17 @@ public class myCalculator {
     public double doAddition(double x, double y){
         return (x + y);
     }
-
+    public double doSubtraction(double x, double y){
+        return (x + y);
+    }
+    public double doMultiplication(double x, double y){
+        return (x * y);
+    }
+    public double doDivision(double x, double y){
+        if( y == 0.0){
+            throw new ArithmeticException();
+        }
+        return ( x/y);
+    }
 }
 
